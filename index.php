56adc6d89300f4a67955c0084b3051f137c605ac
NaufalIhza17 <nawfal17syahzada@gmail.com>
2023-07-28 20:09:22 +0700

    1

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if ( have_posts() ):
         <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
         <?php the_content() ?>
     </article>
-
+    <?php get_template_part('pages/hero') ?>
     <?php endwhile;
 
 else:
