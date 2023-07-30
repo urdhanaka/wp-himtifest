f9f3250fd7aafa1fa9d87c946fbc734d2d044860
urdhanaka <urdhanaka.aptanagi@gmail.com>
2023-07-30 16:29:44 +0700

    fix: change navbar div tag to nav tag

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> 
+<html <?php language_attributes(); ?>>
 
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -11,9 +11,9 @@
 <body <?php body_class(); ?>>
 <div class="container">
     <header>
-        <div class="flex-header">
-            <div><a href="<?php echo home_url() ?>" class="current-link">Beranda</a></div>
-            <div><a href="<?php echo home_url('article') ?>">Artikel</a></div>
-            <div><a href="<?php echo home_url('about') ?>">Tentang Kami</a></div>
-        </div>
+        <nav class="flex-header">
+            <a href="<?php echo home_url() ?>" class="current-link">Beranda</a>  
+            <a href="<?php echo home_url( 'article' ) ?>">Artikel</a>  
+            <a href="<?php echo home_url( 'about' ) ?>">Tentang Kami</a>  
+        </nav>
     </header>
