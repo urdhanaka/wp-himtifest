a9c4fb0d89156e6b80572bb26a181eccd14e5438
NaufalIhza17 <nawfal17syahzada@gmail.com>
2023-07-30 17:47:07 +0700

    hero undone

--- index.php
+++ index.php
@@ -5,11 +5,11 @@ get_header();
 if ( have_posts() ):
     while ( have_posts() ): the_post(); ?>
 
-    <article class="post">
+    <!-- <article class="post">
         <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
         <?php the_content() ?>
-    </article>
-    <?php get_template_part('pages/hero') ?>
+    </article> -->
+    <?php get_template_part('sections/hero') ?>
     <?php endwhile;
 
 else:
---
+++ sections/hero.php
@@ -0,0 +1,10 @@
+<div class="hero">
+    <img width="500" height="600" src='' alt="">
+    <div class="text-container">
+        <h1 class="title-hero">BertigaDjamoe</h1>
+        <p class="desc-hero">Dapatkan rahasia kesehatan alami yang tak ternilai dari artikel djamoe terbaru! Temukan manfaat luar biasa dari ramuan tradisional ini dan jadikan hidup Anda lebih sehat dan bugar.</p>
+        <button class="button-hero">
+            Jelajahi Sekarang
+        </button>
+    </div>
+</div>
