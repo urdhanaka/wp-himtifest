927a17771f45a7142368044ca5c701ea399aaf77
urdhanaka <urdhanaka.aptanagi@gmail.com>
2023-07-30 19:00:26 +0700

    fix: only show hero image on home page

--- index.php
+++ index.php
@@ -2,14 +2,20 @@
 
 get_header();
 
-if ( have_posts() ):
+?>
+
+<?php if ( is_home() ) : ?>
+    <?php get_template_part('sections/hero'); ?>
+<?php endif; ?>
+    
+<?php if ( have_posts() ):
     while ( have_posts() ): the_post(); ?>
 
-    <!-- <article class="post">
+    <article class="post">
         <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
         <?php the_content() ?>
-    </article> -->
-    <?php get_template_part('sections/hero') ?>
+    </article>
+
     <?php endwhile;
 
 else:
