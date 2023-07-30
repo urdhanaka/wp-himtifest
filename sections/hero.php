562849ff7d997427205eaabafcb6f7a4185c6e97
NaufalIhza17 <nawfal17syahzada@gmail.com>
2023-07-30 18:55:20 +0700

    hero done : minus responsive

--- sections/hero.php
+++ sections/hero.php
@@ -1,5 +1,4 @@
 <div class="hero">
-    <img width="500" height="600" src='' alt="">
     <div class="text-container">
         <h1 class="title-hero">BertigaDjamoe</h1>
         <p class="desc-hero">Dapatkan rahasia kesehatan alami yang tak ternilai dari artikel djamoe terbaru! Temukan manfaat luar biasa dari ramuan tradisional ini dan jadikan hidup Anda lebih sehat dan bugar.</p>
