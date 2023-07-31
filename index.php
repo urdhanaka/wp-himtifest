27ec35751866f55f4ed7a43d79a0a413e4b8cd1c
urdhanaka <urdhanaka.aptanagi@gmail.com>
2023-07-31 10:38:22 +0700

    fix: fix header

--- index.php
+++ index.php
@@ -7,22 +7,10 @@ get_header();
 <?php if ( is_home() ) : ?>
     <?php get_template_part('sections/hero'); ?>
 <?php endif; ?>
-    
-<?php if ( have_posts() ):
-    while ( have_posts() ): the_post(); ?>
 
-    <article class="post">
-        <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
-        <?php the_content() ?>
-    </article>
+<?php
 
-    <?php endwhile;
-
-else:
-    echo '<p>There is no post!</p>';
-
-endif;
-
+get_template_part( 'sections/home-about' );
 get_footer();
 
 ?>
