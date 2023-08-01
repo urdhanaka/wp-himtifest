c9f7954fefa157542af1d29857ba4f276086d6ae
urdhanaka <urdhanaka.aptanagi@gmail.com>
2023-08-01 21:41:10 +0700

    feat: add about page

--- page-about.php
+++ page-about.php
@@ -1,18 +1,96 @@
-<?php 
+<?php
 
 get_header();
 
 ?>
 
-<div class="about-hero">
-    <div class="text-container">
-        <h1 class="title-hero">Tentang Jepret Jamu</h1>
-        <p class="desc-hero">Ingin tahu apa itu Jepret jamu ? Ayo scroll ke bawah untuk mengetahui Tentang kami!</p>
+<section class="about-hero">
+    <div class="about-text-container">
+        <h1 class="about-title-hero">Tentang Jepret Jamu</h1>
+        <p class="about-desc-hero">Ingin tahu apa itu Jepret jamu ? Ayo scroll ke bawah untuk mengetahui Tentang kami!</p>
     </div>
-</div>
-<h1>This text -about.php</h1>
+</section>
 
-<?php 
+<section class="about-about">
+    <div class="about-about-text-container">
+        <h1 class="about-about-title">Jepret Jamu</h1>
+        <p class="about-about-desc">
+            Kami adalah tim “BERTIGA”, sebuah kelompok yang dipenuhi semangat dan antusiasme,
+            bertekad untuk mengambil bagian dalam lomba menciptakan website yang unik dan inspiratif
+            dengan tema "Jamu." Dengan hati penuh cinta terhadap budaya Indonesia dan kearifan lokal yang kaya,
+            kami berkomitmen untuk menghadirkan keindahan tradisional jamu dalam bentuk yang segar dan menarik di dunia maya.
+            <br><br>
+            Dalam setiap langkah kami, kami membawa semangat warisan budaya dan nilai-nilai
+            kesehatan alami dari jamu, menjadikan setiap jepretan kami sebagai perwujudan dari kearifan nenek moyang.
+            Kami mengangkat keajaiban jamu, yang telah menjadi bagian integral dari identitas Indonesia selama berabad-abad,
+            dan kami ingin berbagi kekayaan ini dengan seluruh dunia melalui platform website yang kami ciptakan.
+        </p>
+    </div>
+    <div class="about-about-image">
+    </div>
+</section>
+
+<section class="about-member">
+    <h1 class="about-member-title">Anggota Kami</h1>
+    <div class="about-member-members">
+        <div class="members">
+            <div class="photo jeje"></div>
+            <div class="description">
+                <h1 class="member-name">M Naufal Ihza Syahzada</h1>
+                <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <path d="M17 2H7C4.23858 2 2 4.23858 2 7V17C2 19.7614 4.23858 22 7 22H17C19.7614 22 22 19.7614 22 17V7C22 4.23858 19.7614 2 17 2Z" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M16 11.3701C16.1234 12.2023 15.9812 13.0523 15.5937 13.7991C15.2062 14.5459 14.5931 15.1515 13.8416 15.5297C13.0901 15.908 12.2384 16.0397 11.4077 15.906C10.5771 15.7723 9.80971 15.3801 9.21479 14.7852C8.61987 14.1903 8.22768 13.4229 8.09402 12.5923C7.96035 11.7616 8.09202 10.91 8.47028 10.1584C8.84854 9.40691 9.45414 8.7938 10.2009 8.4063C10.9477 8.0188 11.7977 7.87665 12.63 8.00006C13.4789 8.12594 14.2648 8.52152 14.8716 9.12836C15.4785 9.73521 15.8741 10.5211 16 11.3701Z" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M17.5 6.5H17.51" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <path d="M16 8C17.5913 8 19.1174 8.63214 20.2426 9.75736C21.3679 10.8826 22 12.4087 22 14V21H18V14C18 13.4696 17.7893 12.9609 17.4142 12.5858C17.0391 12.2107 16.5304 12 16 12C15.4696 12 14.9609 12.2107 14.5858 12.5858C14.2107 12.9609 14 13.4696 14 14V21H10V14C10 12.4087 10.6321 10.8826 11.7574 9.75736C12.8826 8.63214 14.4087 8 16 8Z" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M6 9H2V21H6V9Z" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M4 6C5.10457 6 6 5.10457 6 4C6 2.89543 5.10457 2 4 2C2.89543 2 2 2.89543 2 4C2 5.10457 2.89543 6 4 6Z" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+        <div class="members">
+            <div class="photo dhani"></div>
+            <div class="description">
+                <h1 class="member-name">Urdhanaka Aptanagi</h1>
+                <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <path d="M17 2H7C4.23858 2 2 4.23858 2 7V17C2 19.7614 4.23858 22 7 22H17C19.7614 22 22 19.7614 22 17V7C22 4.23858 19.7614 2 17 2Z" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M16 11.3701C16.1234 12.2023 15.9812 13.0523 15.5937 13.7991C15.2062 14.5459 14.5931 15.1515 13.8416 15.5297C13.0901 15.908 12.2384 16.0397 11.4077 15.906C10.5771 15.7723 9.80971 15.3801 9.21479 14.7852C8.61987 14.1903 8.22768 13.4229 8.09402 12.5923C7.96035 11.7616 8.09202 10.91 8.47028 10.1584C8.84854 9.40691 9.45414 8.7938 10.2009 8.4063C10.9477 8.0188 11.7977 7.87665 12.63 8.00006C13.4789 8.12594 14.2648 8.52152 14.8716 9.12836C15.4785 9.73521 15.8741 10.5211 16 11.3701Z" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M17.5 6.5H17.51" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <path d="M16 8C17.5913 8 19.1174 8.63214 20.2426 9.75736C21.3679 10.8826 22 12.4087 22 14V21H18V14C18 13.4696 17.7893 12.9609 17.4142 12.5858C17.0391 12.2107 16.5304 12 16 12C15.4696 12 14.9609 12.2107 14.5858 12.5858C14.2107 12.9609 14 13.4696 14 14V21H10V14C10 12.4087 10.6321 10.8826 11.7574 9.75736C12.8826 8.63214 14.4087 8 16 8Z" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M6 9H2V21H6V9Z" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M4 6C5.10457 6 6 5.10457 6 4C6 2.89543 5.10457 2 4 2C2.89543 2 2 2.89543 2 4C2 5.10457 2.89543 6 4 6Z" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+        <div class="members">
+            <div class="photo kevin"></div>
+            <div class="description">
+                <h1 class="member-name">Christian Kevin Emor</h1>
+                <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <path d="M17 2H7C4.23858 2 2 4.23858 2 7V17C2 19.7614 4.23858 22 7 22H17C19.7614 22 22 19.7614 22 17V7C22 4.23858 19.7614 2 17 2Z" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M16 11.3701C16.1234 12.2023 15.9812 13.0523 15.5937 13.7991C15.2062 14.5459 14.5931 15.1515 13.8416 15.5297C13.0901 15.908 12.2384 16.0397 11.4077 15.906C10.5771 15.7723 9.80971 15.3801 9.21479 14.7852C8.61987 14.1903 8.22768 13.4229 8.09402 12.5923C7.96035 11.7616 8.09202 10.91 8.47028 10.1584C8.84854 9.40691 9.45414 8.7938 10.2009 8.4063C10.9477 8.0188 11.7977 7.87665 12.63 8.00006C13.4789 8.12594 14.2648 8.52152 14.8716 9.12836C15.4785 9.73521 15.8741 10.5211 16 11.3701Z" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M17.5 6.5H17.51" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <path d="M16 8C17.5913 8 19.1174 8.63214 20.2426 9.75736C21.3679 10.8826 22 12.4087 22 14V21H18V14C18 13.4696 17.7893 12.9609 17.4142 12.5858C17.0391 12.2107 16.5304 12 16 12C15.4696 12 14.9609 12.2107 14.5858 12.5858C14.2107 12.9609 14 13.4696 14 14V21H10V14C10 12.4087 10.6321 10.8826 11.7574 9.75736C12.8826 8.63214 14.4087 8 16 8Z" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M6 9H2V21H6V9Z" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M4 6C5.10457 6 6 5.10457 6 4C6 2.89543 5.10457 2 4 2C2.89543 2 2 2.89543 2 4C2 5.10457 2.89543 6 4 6Z" stroke="#366C66" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
 
 get_footer()
 
