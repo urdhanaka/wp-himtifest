9fb429066071600ff1f21913c1b4c5a205ab27e0
urdhanaka <urdhanaka.aptanagi@gmail.com>
2023-08-07 07:20:52 +0700

    fix: make all pages Indonesian

---
+++ page-katalog.php
@@ -0,0 +1,13 @@
+<?php 
+
+get_header();
+
+?>
+
+<h1>This text is from page-catalogue</h1>
+
+<?php
+
+get_footer();
+
+?>
---
+++ page-tentang.php
@@ -0,0 +1,11 @@
+<?php
+
+get_header();
+
+get_template_part( 'sections/about-hero' );
+get_template_part( 'sections/about-about' );
+get_template_part( 'sections/about-members' );
+
+get_footer()
+
+?>
